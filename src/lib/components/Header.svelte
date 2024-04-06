0b79afb1ef360f658d62c222a307859406ac117d
Whiteboard Dev <whiteboarddev@pm.me>
2024-04-06 13:57:45 -0300

    feature(svelte): first protoype

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import SocialIcons from './SocialIcons.svelte';
+</script>
+
 <header id="website-header">
 	<nav id="navbar-header" class="nav menu">
 		<img
@@ -6,51 +10,6 @@
 			alt="Whiteboard Dev Logo"
 			src="https://avatars.githubusercontent.com/u/108155624?v=4"
 		/>
-		<ul id="social-links" class="social-links">
-			<li>
-				<a href="https://www.github.com/whiteboardev" target="_blank">
-					<img
-						height="50"
-						width="50"
-						src="/icons/github.svg"
-						alt="Whiteboard Github Account Link"
-						class="social icon"
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://www.linkedin.com/in/joaop-oliveira/" target="_blank">
-					<img
-						height="50"
-						width="50"
-						src="/icons/linkedin.svg"
-						alt="My professional linkedin page"
-						class="social icon linkedin"
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://instagram.com/whiteboarddev" target="_blank">
-					<img
-						height="50"
-						width="50"
-						src="/icons/instagram.svg"
-						alt="Whiteboard Instagram Account Link"
-						class="social icon instagram"
-					/>
-				</a>
-			</li>
-			<li>
-				<a href="https://www.tiktok.com/@whiteboarddev" target="_blank">
-					<img
-						height="50"
-						width="50"
-						src="/icons/tiktok.svg"
-						alt="Whiteboard TikTok Account Link"
-						class="social icon"
-					/>
-				</a>
-			</li>
-		</ul>
+		<SocialIcons className="flex-md" />
 	</nav>
 </header>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import SocialIcons from './SocialIcons.svelte';
+</script>
+
 <main id="main-section" class="container">
 	<!-- TODO: Work on a better hero section copy -->
 	<article id="hero" class="hero">
@@ -7,14 +11,16 @@
 				width="200"
 				alt="Whiteboard Dev Logo"
 				src="https://avatars.githubusercontent.com/u/108155624?v=4"
+				class="hero-image"
 			/>
 		</figure>
-		<h1 id="hero-title">Hello I'm a Front End Developer</h1>
-		<p id="hero-summary">
-			My name is Lenis and I'm a Front End Developer. I'm passionate about web development and I'm
-			always looking for new ways to improve my skills. I'm currently working on a company, but I'm
-			always looking for new opportunities to collaborate with other developers on my free time.
+		<h1 id="hero-title" class="fade-down delay-1s">Hello I'm a Front End Developer</h1>
+		<p id="hero-summary" class="margin-0 fade delay-1-5s">
+			A little bit about me.. I build and maintain web applications for 9 years, Bringing forth
+			expertise in design, installation, testing and maintenance of web systems. Using
+			JavaScript/TypeScript.
 		</p>
-		<button type="button" id="cta-button"> Get in touch!</button>
+		<button type="button" id="cta-button" class="cta-button delay-2s"> Get in touch! </button>
+		<SocialIcons className="flex-sm fade-down delay-2-5s" />
 	</article>
 </main>
