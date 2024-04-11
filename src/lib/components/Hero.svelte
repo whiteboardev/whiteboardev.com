4de3162bd51ce7f1d570638c51f25581e737e7ef
Whiteboard Dev <whiteboarddev@pm.me>
2024-04-10 21:51:25 -0300

    saving icons progress

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,54 +1,9 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import * as Three from 'three';
-	import WebGL from 'three/addons/capabilities/WebGL';
 	import SocialIcons from './SocialIcons.svelte';
-
-	let threeContainer;
-	let scene;
-	let camera;
-	let renderer;
-	let line;
-	let material;
-	let geometry;
-
-	function animate() {
-		requestAnimationFrame(animate);
-	}
-	onMount(() => {
-		if (WebGL.isWebGLAvailable()) {
-			// NOTE: Declaring scene, camera and renderer
-			camera = new Three.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 500);
-			camera.position.set(0, 0, 100);
-			camera.lookAt(0, 0, 0);
-			scene = new Three.Scene();
-			renderer = new Three.WebGLRenderer();
-
-			material = new Three.LineBasicMaterial({ color: 0x0000ff });
-
-			const points = [];
-			points.push(new Three.Vector3(-10, 0, 0));
-			points.push(new Three.Vector3(0, 10, 0));
-			points.push(new Three.Vector3(10, 0, 0));
-
-			geometry = new Three.BufferGeometry().setFromPoints(points);
-
-			line = new Three.Line(geometry, material);
-
-			renderer.setSize(window.innerWidth, window.innerHeight);
-
-			threeContainer.appendChild(renderer.domElement);
-			scene.add(line);
-			renderer.render(scene, camera);
-
-			animate();
-		}
-	});
+	import TechStack from './TechStack.svelte';
 </script>
 
 <main id="main-section" class="container">
-	<div bind:this={threeContainer}></div>
-
 	<!-- TODO: Work on a better hero section copy -->
 	<article id="hero" class="hero">
 		<figure id="hero-image">
@@ -68,5 +23,6 @@
 		</p>
 		<button type="button" id="cta-button" class="cta-button delay-2s"> Get in touch! </button>
 		<SocialIcons className="flex-sm fade-down delay-2-5s" />
+		<TechStack />
 	</article>
 </main>
---
+++ src/lib/components/TechStack.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+</script>
+
+<ul id="tech-stack" class="tech-stack">
+	<li>
+		<img
+			id="js-icon"
+			class="js"
+			height="30"
+			width="30"
+			alt="Javascript Logo"
+			src="/icons/javascript.svg"
+		/>
+	</li>
+	<li>
+		<img
+			id="nodejs-icon"
+			class="nodejs"
+			height="30"
+			width="30"
+			alt="Node.js Logo"
+			src="/icons/nodejs.svg"
+		/>
+	</li>
+	<li>
+		<img
+			id="vue-icon"
+			class="vue"
+			height="30"
+			width="30"
+			alt="Vue.js Logo"
+			src="/icons/vuejs.svg"
+		/>
+	</li>
+	<li>
+		<img id="html-icon" class="html" height="30" width="30" alt="HTML Logo" src="/icons/html.svg" />
+	</li>
+	<li>
+		<img id="css-icon" class="css" height="30" width="30" alt="CSS Logo" src="/icons/css.svg" />
+	</li>
+	<li>
+		<img
+			id="react-icon"
+			class="react"
+			height="30"
+			width="30"
+			alt="React Logo"
+			src="/icons/react.svg"
+		/>
+	</li>
+</ul>
